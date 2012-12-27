f24a2ac0095a3b62efa2c01129186922ed1dff21
Yasuhiro Usutani <y_usutani@me.com>
2012-12-27 18:05:53 +0900

    Cleanup

--- lib/echo.rb
+++ lib/echo.rb
@@ -3,26 +3,28 @@ require 'rubygems'
 require 'em-websocket'
 
 connections = Array.new
-prevMsgs = Array.new
+prev_msgs = Array.new
 
-EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080, :debug => true) do |ws|
-  ws.onopen { 
+EventMachine::WebSocket.start(:host => "0.0.0.0",
+                              :port => 8080,
+                              :debug => true) do |ws|
+  ws.onopen do
     connections.push(ws) unless connections.index(ws)
-    prevMsgs.each {|prevMsg| ws.send(prevMsg)} if ARGV[0] == '--cache'
-  }
+    prev_msgs.each { |msg| ws.send(msg) } if ARGV[0] == '--cache'
+  end
 
-  ws.onmessage {|msg|
+  ws.onmessage do |msg|
     msgs = msg.split(':')
 
     if msgs[0] != '' && ARGV[0] == '--cache'
-      prevMsgs.delete_if{|prevMsg| msg == prevMsg} 
-      prevMsgs.push(msg)
+      prev_msgs.delete_if { |prev_msg| msg == prev_msg } 
+      prev_msgs.push(msg)
     end
 
-    connections.each {|con| con.send(msg) unless con == ws}
-  }
+    connections.each { |con| con.send(msg) unless con == ws }
+  end
 
-  ws.onclose {
-    connections.delete_if{|con| con == ws}
-  }
+  ws.onclose do
+    connections.delete_if { |con| con == ws }
+  end
 end
